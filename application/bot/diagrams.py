5b8f80fb260f4875a638ae901beac542b22baf87
zoomerVAD <zoomervad@yandex.ru>
2024-07-07 20:18:41 +0300

    first commit

--- application/bot/diagrams.py
+++ application/bot/diagrams.py
@@ -1,22 +1,16 @@
 import datetime
-import os
+
 import pathlib
-import sys
 
 import numpy as np
 import matplotlib.pyplot
 import matplotlib.pyplot as plt
 
-sys.path.append(os.path.join(os.getcwd()))
-from chatbot_logger import add_logger, decorator_main_logger
 
 PATH = pathlib.Path(__file__).parent / "files"
 matplotlib.pyplot.set_loglevel(level="warning")
 
-logger = add_logger(__name__)
 
-
-@decorator_main_logger(logger)
 async def get_diagram(idd: int, habits: list[dict[str]]) -> str:
     """Displaying statistics on completed habits in the form of a chart"""
     name_habits = [i["name_habit"] for i in habits]
